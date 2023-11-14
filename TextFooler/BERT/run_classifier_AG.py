8a91744714bf4204970cfb0e8a77ef482bac3377
Aidan-B1409 <aidanb1049@gmail.com>
2023-11-13 18:31:59 -0800

    Update data paths

--- TextFooler/BERT/run_classifier_AG.py
+++ TextFooler/BERT/run_classifier_AG.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir /afs/csail.mit.edu/u/z/zhijing/proj/to_di/data/ag ' \
+command = 'python run_classifier.py --data_dir ../../datasets/adversarial_training_corpora/ag ' \
           '--bert_model bert-base-uncased ' \
           '--task_name ag --output_dir results/ag --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case '
 
--- TextFooler/BERT/run_classifier_Yelp.py
+++ TextFooler/BERT/run_classifier_Yelp.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir /afs/csail.mit.edu/u/z/zhijing/proj/to_di/data/yelp ' \
+command = 'python run_classifier.py --data_dir ../../datasets/adversarial_training_corpora/yelp ' \
           '--bert_model bert-base-uncased --max_seq_length 128 --train_batch_size 32 ' \
           '--task_name yelp --output_dir results/yelp --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case ' \
           '--num_train_epochs 2.'
