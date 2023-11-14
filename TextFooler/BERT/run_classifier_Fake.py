4fbd9c0bdcece1e1a4d02d5d26a1afdc98b05c15
Aidan-B1409 <aidanb1049@gmail.com>
2023-11-13 19:13:19 -0800

    Update data paths

--- TextFooler/BERT/run_classifier_Fake.py
+++ TextFooler/BERT/run_classifier_Fake.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversarial_training_corpora/fake ' \
+command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversary_training_corpora/fake ' \
           '--bert_model bert-base-uncased --max_seq_length 256 --train_batch_size 16 ' \
           '--task_name fake --output_dir results/fake --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case '
 
--- TextFooler/BERT/run_classifier_Yelp.py
+++ TextFooler/BERT/run_classifier_Yelp.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversarial_training_corpora/yelp ' \
+command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversary_training_corpora/yelp ' \
           '--bert_model bert-base-uncased --max_seq_length 128 --train_batch_size 32 ' \
           '--task_name yelp --output_dir results/yelp --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case ' \
           '--num_train_epochs 2.'
