56198e4ac3fa3cb273810ca6462d3937e210523d
Aidan-B1409 <aidanb1049@gmail.com>
2023-11-13 19:10:32 -0800

    Update data paths

--- TextFooler/BERT/run_classifier_AG.py
+++ TextFooler/BERT/run_classifier_AG.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir ../../datasets/adversarial_training_corpora/ag ' \
+command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversarial_training_corpora/ag ' \
           '--bert_model bert-base-uncased ' \
           '--task_name ag --output_dir results/ag --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case '
 
--- TextFooler/BERT/run_classifier_IMDB.py
+++ TextFooler/BERT/run_classifier_IMDB.py
@@ -1,6 +1,6 @@
 import os
 
-command = 'python run_classifier.py --data_dir ../../datasets/adversarial_training_corpora/imdb ' \
+command = 'python run_classifier.py --data_dir /nfs/guille/eecs_research/soundbendor/beerya/CS-579/BERT-Attack/datasets/adversarial_training_corpora/imdb ' \
           '--bert_model bert-base-uncased --max_seq_length 256 --train_batch_size 32 ' \
           '--task_name imdb --output_dir results/imdb --cache_dir pytorch_cache --do_train  --do_eval --do_lower_case ' \
           '--num_train_epochs 3.'
