af4bd413045a4b6f83cbf199956e85a0d4a83f37
Aidan-B1409 <aidanb1049@gmail.com>
2023-12-06 18:51:30 -0800

    Update transformers to break glue

--- transformers/examples/pytorch/text-classification/run_glue.py
+++ transformers/examples/pytorch/text-classification/run_glue.py
@@ -546,10 +546,10 @@ def main():
         )
 
         # Map labels to IDs (not necessary for GLUE tasks)
-        if label_to_id is not None and "label" in examples:
-            result["label"] = [
-                (label_to_id[l] if l != -1 else -1) for l in examples["label"]
-            ]
+        # if label_to_id is not None and "label" in examples:
+        #     result["label"] = [
+        #         (label_to_id[l] if l != -1 else -1) for l in examples["label"]
+        #     ]
         return result
 
     print(raw_datasets["train"])
